3dd3e140372b2e3d9a184d679520cf520f4bcf9b
davyemane <davyemane2@gmail.com>
2025-08-08 14:39:24 +0100

    work on appartement photos

--- apps/menage/urls.py
+++ apps/menage/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     # Check-list simple selon cahier
     path('tache/<int:pk>/checklist/', views.checklist_menage, name='checklist'),
     path('tache/<int:pk>/terminer/', views.terminer_tache, name='terminer'),
+    #planning ménage basique selon cahier
     
     # Création automatique après départ
     path('generer-apres-depart/<int:reservation_pk>/', views.generer_tache_apres_depart, name='generer_apres_depart'),
     
     # Historique
     path('historique/', views.historique_menage, name='historique'),
+    path('programmer/<int:appartement_pk>/', views.programmer_menage, name='programmer'),
 ]
